c5387a74ff086a501661b092cdf5fc1c50faf0e2
abhayworld123 <abhayworld123@gmail.com>
2015-09-11 13:15:06 +0000

    added view temp

--- djn2/urls.py
+++ djn2/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'djn2.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = [
+    
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('marcador.urls')),
-)
+]
+
